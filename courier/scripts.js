b7c426e6d6f188fffb09db771d8991306321eb2f
Javohirshakh <rajabov4you@gmail.com>
2024-07-20 14:10:46 +0500

    added delivery statistics diagram

--- courier/scripts.js
+++ courier/scripts.js
@@ -6,11 +6,12 @@ async function fetchGoogleSheetData(url) {
     return await response.json();
 }
 
-const googleSheetUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=ku7mltm07rRj8Zq3BBufpH4HHyoG4zobdVhRx16hROG-Lt1fjVHwf1Twu54oB5hMmvA4f5Gp4v9II61X1ENty_hGwZReGxxJm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnD1zAvpxYq6r2EMbOrZhm2N-l19A13J-j5l8KBlXdGYQpmrye8wfFofgPewgjubAog9rD9RAr18GNTvyxRgAqfy9I81qUcOFWdz9Jw9Md8uu&lib=MxCcr65cCiDZFciAQak0wvD5qD2GCOdkK';
+const googleSheetUrl = 'https://script.google.com/macros/s/AKfycbwTDCa50yzvvh9-bsXDoK5-livMPyNu1czh2fva1rdi6NH8ond0MuxBEJ5HRXEDv87I3A/exec';
 
 let currentData = [];
 let firstChart;
 let secondChart;
+let deliveryChart;
 
 function dataHasChanged(newData) {
     return JSON.stringify(newData) !== JSON.stringify(currentData);
@@ -38,7 +39,7 @@ async function updateData() {
 
         currentData = newData;
 
-        // Data preparation for first chart
+        // Data preparation for charts
         const dates = newData.map(record => {
             const date = new Date(record.date);
             return date.toLocaleDateString('ru-RU'); // Format date as day.month.year
@@ -46,15 +47,24 @@ async function updateData() {
         const ironData = newData.map(record => record.iron);
         const stateData = newData.map(record => record.state);
         const totalData = newData.map(record => record.total);
+        const onTimeData = newData.map(record => record.onTimeEmployees);
+        const lateData = newData.map(record => record.lateEmployees);
+        const absentData = newData.map(record => record.absentEmployees);
+        const vacationData = newData.map(record => record.vacations);
+        const onTimeDeliveries = newData.map(record => record.onTimeDeliveries);
+        const lateDeliveries = newData.map(record => record.lateDeliveries);
+        const avgDeliveryTime = newData.map(record => record.averageTimeofDeliveries);
 
         updateHeader(dates);
 
         const maxYFirstChart = calculateMaxY([...ironData, ...stateData, ...totalData], 0.3);
+        const maxYSecondChart = calculateMaxY([...onTimeData, ...lateData, ...absentData, ...vacationData], 0.3);
+        const maxYDeliveryChart = calculateMaxY([...onTimeDeliveries, ...lateDeliveries, ...avgDeliveryTime], 0.3);
 
-        // Destroy previous chart if it exists
-        if (firstChart && typeof firstChart.destroy === 'function') {
-            firstChart.destroy();
-        }
+        // Destroy previous charts if they exist
+        if (firstChart && typeof firstChart.destroy === 'function') firstChart.destroy();
+        if (secondChart && typeof secondChart.destroy === 'function') secondChart.destroy();
+        if (deliveryChart && typeof deliveryChart.destroy === 'function') deliveryChart.destroy();
 
         // Create first chart
         firstChart = new Chart(document.getElementById('firstChart'), {
@@ -113,19 +123,6 @@ async function updateData() {
             plugins: [ChartDataLabels]
         });
 
-        // Data preparation for second chart
-        const onTimeData = newData.map(record => record.onTimeEmployees);
-        const lateData = newData.map(record => record.lateEmployees);
-        const absentData = newData.map(record => record.absentEmployees);
-        const vacationData = newData.map(record => record.vacations);
-
-        const maxYSecondChart = calculateMaxY([...onTimeData, ...lateData, ...absentData, ...vacationData], 0.3);
-
-        // Destroy previous chart if it exists
-        if (secondChart && typeof secondChart.destroy === 'function') {
-            secondChart.destroy();
-        }
-
         // Create second chart
         secondChart = new Chart(document.getElementById('secondChart'), {
             type: 'bar',
@@ -142,22 +139,22 @@ async function updateData() {
                     {
                         label: 'Kechikkanlar',
                         data: lateData,
-                        backgroundColor: 'rgb(255, 0, 0)',
+                        backgroundColor: 'rgba(255, 0, 0, 0.8)',
                         borderColor: 'rgba(220, 20, 60, 1)',
                         borderWidth: 1
                     },
                     {
                         label: 'Ishga chiqmaganlar',
                         data: absentData,
-                        backgroundColor: 'rgb(0, 0, 0)',
-                        borderColor: 'rgb(0, 0, 0)',
+                        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                        borderColor: 'rgba(0, 0, 0, 1)',
                         borderWidth: 1
                     },
                     {
                         label: 'Dam olganlar',
                         data: vacationData,
                         backgroundColor: 'rgba(255, 165, 0, 0.8)',
-                        borderColor: 'rgba(255, 165, 0, 0.8)',
+                        borderColor: 'rgba(255, 165, 0, 1)',
                         borderWidth: 1
                     }
                 ]
@@ -168,6 +165,63 @@ async function updateData() {
                     y: {
                         beginAtZero: true,
                         max: maxYSecondChart
+                    }
+                },
+                layout: {
+                    padding: {
+                        bottom: 20 // Добавляем нижний отступ
+                    }
+                },
+                plugins: {
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'end',
+                        formatter: (value) => Math.round(value),
+                        color: '#000',
+                        padding: {
+                            top: 10 // Добавляем верхний отступ для меток данных
+                        }
+                    }
+                }
+            },
+            plugins: [ChartDataLabels]
+        });
+
+        // Create delivery chart
+        deliveryChart = new Chart(document.getElementById('deliveryChart'), {
+            type: 'bar',
+            data: {
+                labels: dates,
+                datasets: [
+                    {
+                        label: 'Vaqtida yetkazib berilgan',
+                        data: onTimeDeliveries,
+                        backgroundColor: 'rgba(34, 139, 34, 0.8)',
+                        borderColor: 'rgba(34, 139, 34, 1)',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Kechikib yetkazib berilgan',
+                        data: lateDeliveries,
+                        backgroundColor: 'rgba(255, 0, 0, 0.8)',
+                        borderColor: 'rgba(220, 20, 60, 1)',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Oʻrtacha yetkazib berish vaqti',
+                        data: avgDeliveryTime,
+                        backgroundColor: 'rgba(75, 192, 192, 0.8)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        max: maxYDeliveryChart
                     }
                 },
                 layout: {
