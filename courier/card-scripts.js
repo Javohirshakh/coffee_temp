a147a2b70246243bf286de2decf52d9a1d727b21
Javohirshakh <rajabov4you@gmail.com>
2024-07-20 19:25:38 +0500

    changed sizes

--- courier/card-scripts.js
+++ courier/card-scripts.js
@@ -48,14 +48,14 @@ async function updateCards() {
         ];
 
         cards.forEach(card => {
-            const improvementClass = card.reverse ? (card.improvement ? 'decline' : 'improvement') : (card.improvement ? 'improvement' : 'decline');
+            const improvementClass = card.reverse ? (card.improvement ? 'improvement' : 'decline') : (card.improvement ? 'improvement' : 'decline');
             const cardElement = document.createElement('div');
             cardElement.className = 'card p-4 bg-white rounded-lg shadow-lg flex flex-col items-center justify-between';
             cardElement.innerHTML = `
                 <h4 class="font-bold text-base mb-2">${card.title}</h4>
                 <div class="flex items-center justify-between w-full mt-1">
                     <p class="current text-sm/[16px] font-medium whitespace-normal ${improvementClass}">${card.value}</p>
-                    <p class="previous text-sm/[8px] font-normal previous-style">${card.previous}</p>
+                    <p class="previous text-sm/[8px] font-normal previous">${card.previous}</p>
                 </div>`;
             cardsContainer.appendChild(cardElement);
         });
